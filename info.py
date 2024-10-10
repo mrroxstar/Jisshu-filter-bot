372f21ce79126b3fdcf4c4057ef0c7b025a57fdd
mrroxstar <146523615+mrroxstar@users.noreply.github.com>
2024-10-10 16:44:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "brave-donni-narutokd-ce8f377b.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
